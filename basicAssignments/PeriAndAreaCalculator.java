b50313017ad41d81d6260e6ade14231094bebce7
İbrahim Halil Han <103876746+suleykaar@users.noreply.github.com>
2023-05-16 10:13:06 +0300

    center of angle info added.

--- basicAssignments/PeriAndAreaCalculator.java
+++ basicAssignments/PeriAndAreaCalculator.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class PeriAndAreaCalculator {
     public static void main(String[] args) {
-        int r;
+        int r, c;
         double pi = 3.14;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the radius of the circle: ");
-
         r = input.nextInt();
+        System.out.println("Enter the center angle of the circle: ");
+        c = input.nextInt();
         System.out.println("Perimeter of the circle: " + 2 * pi * r);
-        System.out.print("Area of the circle: " + pi * r*r);
+        System.out.print("Area of the circle: " + (pi * r*r * c) / 360);
 
         input.close();
     }
